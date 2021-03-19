4b18ef2e3843687f2bfb1d6fbc41ac95e6800fa9
GohanSebas <gohansebas_94@hotmail.com>
2021-03-18 21:30:34 -0500

    prueba2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>sebastian prueba</code> 
+          <code>sebastian prueba 2</code> 
         </p>
         <a
           className="App-link"
